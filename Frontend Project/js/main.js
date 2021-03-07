3a4dd138ade3ae61df349316ebe95c3415ca8900
Nihad <nihadvos@code.edu.az>
2021-03-07 05:55:43 -0800

    header and carousel

---
+++ Frontend Project/js/main.js
@@ -0,0 +1,5 @@
+// let navAction = document.querySelector(".nav-action");
+// navAction.addEventListener("click", function () {
+//   document.getElementsByClassName(".nav-title").style.display = "block";
+//   console.log("hey");
+// });
